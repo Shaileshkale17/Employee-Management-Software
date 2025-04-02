ac8c5e845b8caa028b207e5d2a61f207186de3de
shailesh kale <shaileshkale87730@gmail.com>
2025-04-03 00:36:47 +0530

    adding the footer text

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <div className="h-10 text-center bg-white  border border-solid border-[#3354F4] shadow-lg drop-shadow-lg ">
-      Footer
+      All Copyright Reserved By ©{}{" "}
+      <Link
+        to="https://protfolio-shailesh-full-stack-developer.vercel.app/"
+        className="cursor-pointer">
+        Shailesh Kale
+      </Link>
     </div>
   );
 };
