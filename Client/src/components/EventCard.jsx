cb91cff505e07df74e4d2a9fcaf3c995c3b5f8e4
shailesh kale <shaileshkale87730@gmail.com>
2025-04-10 22:31:22 +0530

    adding the event page

--- Client/src/components/EventCard.jsx
+++ Client/src/components/EventCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const EventCard = ({ tasks }) => {
   return (
-    <div className=" grid col-span-3 grid-cols-3 gap-3">
+    <div className=" grid col-span-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 xl:grid-cols-4">
       {tasks.map((item, index) => (
         <div
           key={index}
